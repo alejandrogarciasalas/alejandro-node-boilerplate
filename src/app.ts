4cb08b751418e820cf832bdb09b52cd29a3c5510
Alejandro Garcia Salas <garciasalasalejandro@gmail.com>
2021-03-28 16:57:24 -0700

    nits

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,9 @@ dotenv.config();
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.get('/', (res) => {
-  res.send('hello world');
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
-
 app.listen(port, () => {
   console.log(`app listening at http://localhost:${port}`);
 });
